39df4fba0ee403df23a927c359457d83255c5fbd
surafel fekadu <surafelfekadu399@gmail.com>
2023-10-31 15:53:50 +0300

    (Problems) A Clouser: where an inner function has access to the outer (enclosing) function’s variables — a scope chain.

---
+++ counter.js
@@ -0,0 +1,25 @@
+// Given an integer n, return a counter function. 
+// This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    var firstTime = true;
+    return function() {
+        if(firstTime) {
+            firstTime = false;
+            return n;
+        } else {
+            n = n + 1;
+            return n;
+        }
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
